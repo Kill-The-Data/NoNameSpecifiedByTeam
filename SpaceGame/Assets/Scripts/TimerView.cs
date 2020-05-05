795c2e0987d70a4bc2c259ca6003ff3c074da63c
Lukas <469260@student.saxion.nl>
2020-05-05 12:12:44 +0200

    Basic pick up and ui

---
+++ SpaceGame/Assets/Scripts/TimerView.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public interface IObserver
+{
+     void GetUpdate(ISubject subject);
+
+}
+public interface ISubject
+{
+     void Notify();
+     void Attach(IObserver observer);
+}
+
+
+public class TimerView : MonoBehaviour, IObserver
+{
+    [SerializeField] private TMP_Text _Text;
+    [SerializeField] private Timer _timer;
+    [SerializeField] private float _StartDuration;
+    private void Start()
+    {
+        if (_timer) 
+        {
+            _timer.Attach(this);
+            _timer.StartTimer(_StartDuration);
+        }
+    }
+    public void GetUpdate()
+    {
+    }
+
+    public void GetUpdate(ISubject subject)
+    {
+
+        if (subject is Timer)
+        {
+            Timer timer = subject as Timer;
+            if (timer.GetState() == Timer.TimerState.ACTIVE) 
+            {
+                float timeLeft = timer.GetTime();
+                UpdateText(timeLeft);
+            }
+            else if (timer.GetState() == Timer.TimerState.OUT_OF_TIME) 
+            {
+                UpdateText(0);
+            }
+
+        }
+    }
+    private void UpdateText(float time)
+    {
+        int timeRounded = Mathf.RoundToInt(time);
+        string message = timeRounded.ToString();
+
+        _Text.SetText(message);
+    }
+}
---
+++ SpaceGame/Assets/Scripts/TrashCollisionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCollisionHandler : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent.GetComponent<PlayerCargo>())
+        {
+            PlayerCargo playerCargo = other.transform.parent.GetComponent<PlayerCargo>();
+            if (!playerCargo.SpaceIsFull()) 
+            {
+               Destroy(this.transform.gameObject);
+                playerCargo.AddCargo();
+            }
+        }
+    }
+}
