279700928257760296dcbe692557f22b2a6aea60
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-05-06 11:09:37 +0200

    added ability to hail osk (only windows,remove in other builds!)

---
+++ SpaceGame/Assets/Scripts/HailOSK.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HailOSK : MonoBehaviour
+{
+    public enum Method
+    {
+        TabTip,
+        OSK
+    }
+
+    [Header(" --- Setup ---")]
+
+    [Tooltip("The keyboard to hail")]
+    public Method method = Method.TabTip;
+
+    public void Hail()
+    {
+        //hail the on screen keyboard, for windows 10 tabtip is the new method
+        System.Diagnostics.Process.Start(method == Method.TabTip ? "tabtip.exe" : "osk.exe");
+    }
+}
---
+++ SpaceGame/Assets/Scripts/HailOSKEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(HailOSK))]
+public class HailOSKEditor : Editor
+{
+    private HailOSK.Method m_method;
+
+    public override void OnInspectorGUI()
+    {
+
+
+
+        (target as HailOSK).method = (HailOSK.Method)EditorGUILayout.EnumFlagsField("Method", (target as HailOSK).method) ;
+
+        if (GUILayout.Button("Hail Keyboard"))
+        {
+            (target as HailOSK)?.Hail();
+        }
+    }
+}
