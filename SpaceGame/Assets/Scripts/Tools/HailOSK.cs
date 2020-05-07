2435c1bf6c1acb7a179243972aef4f3a56ee455b
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-05-07 09:18:47 +0200

    increased the usefullness of the osk-hailing method for tabtip

--- SpaceGame/Assets/Scripts/Tools/HailOSK.cs
+++ SpaceGame/Assets/Scripts/Tools/HailOSK.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class HailOSK : MonoBehaviour
@@ -19,5 +19,28 @@ public class HailOSK : MonoBehaviour
     {
         //hail the on screen keyboard, for windows 10 tabtip is the new method
         System.Diagnostics.Process.Start(method == Method.TabTip ? "tabtip.exe" : "osk.exe");
+        if (method == Method.TabTip)
+        {
+            var uiHostNoLaunch = new UIHostNoLaunch();
+            var tipInvocationElement = uiHostNoLaunch as ITipInvocation;
+            tipInvocationElement?.Toggle((GetDesktopWindow()));
+            Marshal.ReleaseComObject(uiHostNoLaunch);
+
+        }
     }
+
+    [ComImport, Guid("4ce576fa-83dc-4F88-951c-9d0782b4e376")]
+    class UIHostNoLaunch
+    {
+    }
+
+    [ComImport, Guid("37c994e7-432b-4834-a2f7-dce1f13b834b")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface ITipInvocation
+    {
+        void Toggle(IntPtr hwnd);
+    }
+
+    [DllImport("user32.dll", SetLastError = false)]
+    static extern IntPtr GetDesktopWindow();
 }
