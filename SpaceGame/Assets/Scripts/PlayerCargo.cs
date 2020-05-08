05a7b328ca8385c9ef309ceffa081973e41848dd
Lukas <469260@student.saxion.nl>
2020-05-08 10:31:56 +0200

    polished & fixed slider, now displays text again,  does not update every frame, added min slider value so it does not disappear

--- SpaceGame/Assets/Scripts/PlayerCargo.cs
+++ SpaceGame/Assets/Scripts/PlayerCargo.cs
@@ -11,9 +11,12 @@ public class PlayerCargo : MonoBehaviour
     [SerializeField] private Slider m_slider = null;
 
 
-    [Header(" --- Tween setup ---")]
+    [Header(" --- Slider UI setup ---")]
     [Range(0, 1.5f)]
     [SerializeField] private float m_tweenSpeed = 1.0f;
+    [Tooltip("The slider should never reach 0, only close to 0 so that it does not disappear")]
+    [Range(0.01f,0.2f)]
+    [SerializeField] private float m_MinSliderValue = 0.1f;
 
     [Header(" --- Cargo ---")]
     [Tooltip("How much items the Player can hold at any given time")]
@@ -48,15 +51,10 @@ public class PlayerCargo : MonoBehaviour
         m_slider.maxValue = m_cargoLimit;
         m_slider.minValue = 0;
         m_lerp = m_slider.gameObject.AddComponent<LerpSlider>();
-        m_lerp.Init(m_slider, m_tweenSpeed);
+        m_lerp.Init(m_slider, m_tweenSpeed, m_MinSliderValue);
     }
     
-    //Update the Slider & Text
-    //TODO(kukash): see below
-    private void Update()
-    {
-        UpdateView();
-    }
+ 
 
     //check if space is full and otherwise add n element to the inventory
     public void AddCargo(int amount = 1)
@@ -88,10 +86,22 @@ public class PlayerCargo : MonoBehaviour
     //update the text & the slider to reflect the status of the inventory
     private void UpdateView()
     {
-        //TODO(kukash): reimplement the text and split this function up into 
-        //TODO(cont.): UpdateText() and UpdateSlider() so that the get=> property 
-        //TODO(cont.): SpaceOccupied saves us some computation time again!
-        //m_text.SetText($"{SpaceOccupied} / {m_cargoLimit}");
+        UpdateSlider();
+        UpdateText();
+    }
+    private void UpdateSlider() 
+    {
         m_lerp.UpdateSlider(SpaceOccupied);
+    }
+    private void UpdateText()
+    {
+        if (SpaceOccupied != 0)
+        {
+            m_text.SetText($"{SpaceOccupied} / {m_cargoLimit}");
+        }
+        else
+        {
+            m_text.SetText("");
+        }
     }
 }
