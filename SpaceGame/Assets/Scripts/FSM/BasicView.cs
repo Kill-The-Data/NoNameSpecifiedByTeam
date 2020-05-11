1692427cf346a5cbfa3808a6175852aeb4d84835
Lukas <469260@student.saxion.nl>
2020-05-11 10:45:01 +0200

    Basic fsm main see main menu example, play button loads ingame ui

---
+++ SpaceGame/Assets/Scripts/FSM/BasicView.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicView : AbstractView
+{
+   //basic view does not do anything besides enable & disable from base class
+}
