2022fdea4edd6b91a35ac6284607a4c670dd342a
Lukas <469260@student.saxion.nl>
2020-05-13 09:39:43 +0200

    Ingame timer now notyfies deathwatch, causes game over once out of time

--- SpaceGame/Assets/Scripts/Tools/DeathWatch.cs
+++ SpaceGame/Assets/Scripts/Tools/DeathWatch.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathWatch : MonoBehaviour
+public class DeathWatch : MonoBehaviour, IObserver
 {
     private bool m_hasStarted = false;
 
     public PlayerHealth PlayerHealth;
-    public State State;    
-    
+    public State State;
+    public Timer timer;
+
     void StartDeathWatch()
     {
         m_hasStarted = true;
@@ -16,10 +17,25 @@ public class DeathWatch : MonoBehaviour
 
     void Update()
     {
-        if (PlayerHealth && PlayerHealth.IsDead() )
+        if (PlayerHealth && PlayerHealth.IsDead())
         {
-            if(State is IngameState ingameState)  ingameState.PlayerDied();
-            else if(State is TutorialState tutorialState) tutorialState.PlayerDied();
+            GameOver();
+        }
+    }
+    private void GameOver()
+    {
+        if (State is IngameState ingameState) ingameState.PlayerDied();
+        else if (State is TutorialState tutorialState) tutorialState.PlayerDied();
+    }
+    //gets notified by timer
+    public void GetUpdate(ISubject subject)
+    {
+        if (subject is Timer timer)
+        {
+            if (timer.GetState() == Timer.TimerState.OUT_OF_TIME) 
+            {
+                GameOver();
+            }
         }
     }
 }
