56d3c564ba670653964045d55778c035907f7b25
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-05-18 14:17:14 +0200

    Observer-patternified all the things, added the unfathomably loud collision sound

--- SpaceGame/Assets/DebrisCollisionLinker.cs
+++ SpaceGame/Assets/DebrisCollisionLinker.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using Tools;
 using UnityEngine;
 
-public class DebrisCollisionLinker : MonoBehaviour
+public class DebrisCollisionLinker : AUnityObserver
 {
+    [SerializeField] private AUnityObserver m_target;
+    [SerializeField] private GameObject m_debrisField;
+    private NotifyAddChildren m_nac;
 
-    [SerializeField,SerializeReference] public ISubject m_subject;
+    private void Awake()
+    {
+        if (m_debrisField.GetComponentSafe(out NotifyAddChildren nac))
+        {
+            nac.Attach(this);
+            m_nac = nac;
+        }
+    }
+
+    protected override void AGetUpdate(ISubject subject)
+    {
+        if(m_nac.LastAdded.GetComponentSafe(out TrashCollisionHandler tch))
+        {
+            tch.playerTakeDamageFilter.Attach(m_target);
+        }
+    }
 }
