cbf0a631ffe930ac5d240864afe0811e8f6756a4
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-05-18 15:14:40 +0200

    added bank Angle feature

---
+++ SpaceGame/Assets/Scripts/Tools/QuaternionIsNanExt.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public static class QuaternionIsNanExt
+{
+    public static bool IsNaN(this Quaternion q) {
+        return float.IsNaN(q.x) || float.IsNaN(q.y) || float.IsNaN(q.z) || float.IsNaN(q.w);
+    }
+}
