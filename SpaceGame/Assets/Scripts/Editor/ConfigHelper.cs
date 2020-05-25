0914bcb82ea83125c81e8740c3664b66712cfba6
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-05-25 10:47:19 +0200

    added online cfg system

---
+++ SpaceGame/Assets/Scripts/Editor/ConfigHelper.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+public class ConfigHelper : EditorWindow
+{
+    [MenuItem("Window/ConfigHelper")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<ConfigHelper>();
+    }
+
+    private void OnGUI()
+    {
+        //fetch data and check if we can query a field
+        if (GUILayout.Button("Test Fetch"))
+        {
+            var conf = WebConfigHandler.GetConfig();
+            Debug.Log(conf["health"]);
+        }
+    }
+}
---
+++ SpaceGame/Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Security.Cryptography;
+
+public static class Test
+{
+    private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+    public static byte[] MakeABunchOfBytes()
+    {
+        byte[] blob = new byte[64];
+        rngCsp.GetBytes(blob);
+        return blob;
+    }
+}
