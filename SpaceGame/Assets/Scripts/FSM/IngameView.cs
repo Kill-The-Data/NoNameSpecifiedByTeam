e9958370e312ad5f1c762286a7bcbf0331f29241
Lukas <469260@student.saxion.nl>
2020-05-28 14:28:14 +0200

    Added required components & removed few usings

--- SpaceGame/Assets/Scripts/FSM/IngameView.cs
+++ SpaceGame/Assets/Scripts/FSM/IngameView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(TimeOutTimer))]
 public class IngameView : AbstractView
 {
 
--- SpaceGame/Assets/Scripts/FinishCollisionHandler.cs
+++ SpaceGame/Assets/Scripts/FinishCollisionHandler.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class FinishCollisionHandler : MonoBehaviour
 {
 
