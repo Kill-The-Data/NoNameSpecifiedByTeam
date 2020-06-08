e1d35efd9d9d5d6c400dab468a9c48b14d20f364
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-06-08 15:05:04 +0200

    no more endless highscore names

--- SpaceGame/Assets/Scripts/FSM/EnterNameView.cs
+++ SpaceGame/Assets/Scripts/FSM/EnterNameView.cs
@@ -1,8 +1,14 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnterNameView : AbstractView
 {
+    [SerializeField] private Button m_button;
     [SerializeField] private TMP_Text m_text;
     public string GetText() => m_text.text;
+
+    public void DisableButton() => m_button.enabled = false;
+    public void EnableButton() => m_button.enabled = true;
+
 }
