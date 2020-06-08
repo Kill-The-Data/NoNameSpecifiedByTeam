0fe15ca066b7fcd306103abd3a3de1e8dbe18935
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-06-08 09:31:32 +0200

    fixed cargo vis and moved files to proper locations

---
+++ SpaceGame/Assets/Scripts/PlayerScripts/PlayerCargoVisual.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerCargoVisual : MonoBehaviour
+{
+    [SerializeField] private GameObject m_targetParent = null;
+
+    public void InstantiateObj(GameObject obj)
+    {
+        GameObject newObj = Instantiate(obj);
+        if (m_targetParent)
+        {
+            newObj.transform.parent = m_targetParent.transform;
+            newObj.transform.localScale = Vector3.one;
+            newObj.transform.localPosition = Vector3.zero;
+        }
+
+        
+        
+        //since TrashCollisionHandler depends on TrashMovementController
+        //it needs to be removed before everything else
+        //
+        Destroy(newObj.GetComponent(typeof(TrashCollisionHandler)));
+        
+        Component[] components = newObj.GetComponents<Component>();
+        
+        foreach (Component currentComponent in components)
+        {
+            if (currentComponent is Rigidbody
+                || currentComponent is Collider
+                || currentComponent is nextTutorialState
+                || currentComponent is BreakApartHandler
+                || currentComponent is TrashMovementController
+            )
+            {
+                Destroy(currentComponent);
+            }
+        }
+    }
+
+    public void RemoveObj(int amount)
+    {
+        for (int i = 0; i < m_targetParent.transform.childCount; ++i)
+            if (amount <= i)
+                Destroy(m_targetParent.transform.GetChild(i).gameObject);
+    }
+}
