cdb1cfb25a0b8fade65df1441ed42a1b5a799c68
lukas hausmann <469260@student.saxion.nl>
2020-06-10 15:17:04 +0200

    fixed some issues now scales with screen height

--- SpaceGame/Assets/CinematicController.cs
+++ SpaceGame/Assets/CinematicController.cs
@@ -2,12 +2,14 @@
 
 public class CinematicController : MonoBehaviour
 {
+    [Header("---assign bar ui images---")]
 
     [SerializeField] private GameObject m_UpperBar = null;
     [SerializeField] private GameObject m_LowerBar = null;
-
-    [SerializeField] private float m_invisPos = 0;
-    [SerializeField] private float m_visPos = 0;
+    [Header("---should not be adjusted---")]
+    [SerializeField] private float m_offset = 69;
+    private float m_YPos = 0;
+    [Header("---setup tween---")]
     [SerializeField] private float m_TweenSpeed = 0.75f;
     [SerializeField] private LeanTweenType m_TweenType = LeanTweenType.linear;
 
@@ -18,26 +20,31 @@ public class CinematicController : MonoBehaviour
 
     void Update()
     {
-        //if (Input.GetKeyDown("space"))
-        //    LerpIn();
-        //if (Input.GetKeyDown("="))
-        //    LerpOut();
+
+#if UNITY_EDITOR
+        if (Input.GetKeyDown("space"))
+            LerpIn();
+        if (Input.GetKeyDown("="))
+            LerpOut();
+#endif
     }
 
     //THIS DOES ONLY WORK FOR FIXED RESOLUTION 
     private void LerpIn()
     {
-        m_UpperBar.LeanMoveY(Screen.height - m_visPos, m_TweenSpeed).setEase(m_TweenType);
-        m_LowerBar.LeanMoveY(m_visPos, m_TweenSpeed).setEase(m_TweenType);
+        m_UpperBar.LeanMoveY(Screen.height - m_YPos, m_TweenSpeed).setEase(m_TweenType);
+        m_LowerBar.LeanMoveY(m_YPos, m_TweenSpeed).setEase(m_TweenType);
     }
     private void LerpOut()
     {
-        m_UpperBar.LeanMoveY(Screen.height + m_visPos, m_TweenSpeed).setEase(m_TweenType);
-        m_LowerBar.LeanMoveY(-m_invisPos, m_TweenSpeed).setEase(m_TweenType);
+        m_UpperBar.LeanMoveY(Screen.height + m_YPos, m_TweenSpeed).setEase(m_TweenType);
+        m_LowerBar.LeanMoveY(-m_YPos, m_TweenSpeed).setEase(m_TweenType);
     }
     private void Reset()
     {
-        m_UpperBar.transform.position = new Vector3(Screen.width / 2, Screen.height + m_visPos, 0);
-        m_LowerBar.transform.position = new Vector3(Screen.width / 2 - m_visPos, --m_invisPos, 0);
+        m_YPos = m_offset * Screen.height / 1080;
+      
+        m_UpperBar.transform.position = new Vector3(Screen.width / 2, Screen.height + m_YPos, 0);
+        m_LowerBar.transform.position = new Vector3(Screen.width / 2, -m_YPos, 0);
     }
 }
