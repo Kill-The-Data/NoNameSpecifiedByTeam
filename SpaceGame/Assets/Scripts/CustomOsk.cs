4a83d51f8fabb73014ba81dabca6eaa6bf5a8011
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-06-15 10:08:26 +0200

    made custom osk, not gonna do that again

---
+++ SpaceGame/Assets/Scripts/CustomOsk.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomOsk : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField m_text;
+
+    private Button[] m_buttons;
+    
+    void InsertKeyAction(string character)
+    {
+        m_text.text += character;
+    }
+
+    void DelKeyAction()
+    {
+        if (m_text.text.Length > 0)
+        {
+            m_text.text = m_text.text.Substring(0, m_text.text.Length - 1);
+        }
+    }
+    
+    void Start()
+    {
+        m_buttons = GetComponentsInChildren<Button>();
+        foreach (var button in m_buttons)
+        {
+            if (button.name != "Del" && button.name != "Space")
+            {
+                button.onClick.AddListener(() => InsertKeyAction(button.name));
+            }
+            else if (button.name == "Space")
+            {
+                button.onClick.AddListener(()=>InsertKeyAction(" "));
+            }
+            else
+            {
+                button.onClick.AddListener(DelKeyAction);
+            }
+        }
+    }
+}
