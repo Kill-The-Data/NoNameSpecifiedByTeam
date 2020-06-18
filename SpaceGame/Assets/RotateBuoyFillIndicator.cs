89b928c3c6fab35a7a49f446bb0f7d21e90f49e3
kukash <469260@student.saxion.l>
2020-06-18 16:44:42 +0200

    stuff

---
+++ SpaceGame/Assets/RotateBuoyFillIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBuoyFillIndicator : MonoBehaviour
+{
+
+    public float angle = 0;
+    public Transform center;
+    public Vector3 rotationCenter = new Vector3(0, 0, 0);
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            angle = Mathf.Lerp(angle, angle + 10, 10);
+        }
+        //transform.RotateAround(Vector3.forward, angle);
+        transform.localRotation = Quaternion.EulerAngles(0, angle, 0);
+    }
+
+    public void UpdateRotation()
+    {
+
+    }
+
+}
