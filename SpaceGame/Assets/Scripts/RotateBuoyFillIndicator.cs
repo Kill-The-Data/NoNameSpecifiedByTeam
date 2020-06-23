2cb06c3c6050dde4a87f65ccdf5c09487406845f
kukash <469260@student.saxion.l>
2020-06-23 09:55:01 +0200

    disabled "cast shadow" for most stuff, my gpu now does not get fried anymore

--- SpaceGame/Assets/Scripts/RotateBuoyFillIndicator.cs
+++ SpaceGame/Assets/Scripts/RotateBuoyFillIndicator.cs
@@ -1,16 +1,12 @@
-﻿﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.PlayerLoop;
-
+﻿using UnityEngine;
 public class RotateBuoyFillIndicator : MonoBehaviour
 {
     [SerializeField]private  UpdateBuoyText m_buoyText = null;
+    [SerializeField] private bool m_RotateComponent = false;
     public void UpdateRotation(float fillAmount)
     {
         m_buoyText?.UpdateText(fillAmount);
-
+        if (!m_RotateComponent) return;
         var rotation = fillAmount * -360;
         transform.localRotation = Quaternion.Euler(0, Mathf.Clamp(rotation, -360, 0), 0);
     }
@@ -21,6 +17,5 @@ public class RotateBuoyFillIndicator : MonoBehaviour
     private void Reset()
     {
         transform.localRotation = Quaternion.Euler(0, 0, 0);
-     
     }
 }
