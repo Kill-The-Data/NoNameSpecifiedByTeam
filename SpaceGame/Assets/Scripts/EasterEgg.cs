cedfe31c3adc888bcbf3dac3cb1f2a599b5f8e28
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-06-23 16:15:51 +0200

    added the same system from mail for easter eggs, but without a top limit

---
+++ SpaceGame/Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using UnityEngine;
+
+public class EasterEgg :MonoBehaviour
+{
+    public event Action OnEasterEggPickedUp;
+
+    private void Start()
+    {
+        EastereggCounter.OnInstance(instance =>
+        {
+            OnEasterEggPickedUp += instance.AddEasterEgg;
+        });
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Player-Collector"))
+        {
+            OnEasterEggPickedUp?.Invoke();
+        }
+    }
+}
