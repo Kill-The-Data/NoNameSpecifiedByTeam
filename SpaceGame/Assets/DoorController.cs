991edec44b92ac9d8b1a5d38d7013e67d16bb0cf
kukash <469260@student.saxion.l>
2020-06-23 12:02:18 +0200

    Fixed script, door now can kill you, pls check no kill funcionallity once merged

--- SpaceGame/Assets/DoorController.cs
+++ SpaceGame/Assets/DoorController.cs
@@ -1,58 +1,66 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    //prefabs to instantiate 
+    [Header("---Door prefabs---")]
     [SerializeField] private GameObject m_firstDoorPrefab = null;
     [SerializeField] private GameObject m_secondDoorPrefab = null;
-
-    [SerializeField] private GameObject m_FirstDoor = null;
-    [SerializeField] private GameObject m_SecondDoor = null;
+    //obejcts to store instantiated prefabs in 
+    private GameObject m_FirstDoor = null;
+    private GameObject m_SecondDoor = null;
+    //tween stuff
+    [Header("---Setup Tweening---")]
     [SerializeField] private float m_moveDistance = 25.0f;
-    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private LeanTweenType m_TweenType = LeanTweenType.easeOutExpo;
-
+    //index to destroy the right door
     private int doorIndex = 0;
+
+    //gets called by tutorial state on buoy filled,
+    //opens door & increases index for destroying the right door
     public void OpenFirstDoor()
     {
         if (m_FirstDoor)
-        { 
+        {
             OpenDoor(m_FirstDoor.transform);
             doorIndex++;
         }
     }
+    //gets called by tutorial state on tutorial finished,
+    //opens door & increases index for destroying the right door
     public void OpenSecondDoor()
     {
         if (m_SecondDoor)
-        { 
+        {
             OpenDoor(m_SecondDoor.transform);
             doorIndex++;
         }
     }
-    //decide which object is above or below center of door
+    //"opens" door => moves it, fades it out , destroys it on complete
     //pls make sure that door objects are positioned properly
     private void OpenDoor(Transform targetTransform)
     {
         foreach (Transform child in targetTransform)
         {
+            //decide on the move direction
             int dir = 1;
             if (child.transform.localPosition.y < 0) dir = -1;
 
+            //move Object
             child.LeanMoveLocalY(child.transform.localPosition.y + m_moveDistance * dir, moveSpeed).setEase(m_TweenType).setOnComplete(DestroyDoor);
 
             //Fade alpha
+            Renderer r = child.GetComponent<Renderer>();
             LeanTween.value(child.gameObject, 1, 0, moveSpeed).setEase(m_TweenType).setOnUpdate((float val) =>
             {
-                Renderer r = child.GetComponent<Renderer>();
-
                 r.material.SetFloat("ALPHA", val);
             });
         }
     }
-    private void DestroyObject(GameObject obj)
-    {
-        Destroy(obj);
-    }
+    //destroys doors incrementally, depending on the index
     private void DestroyDoor()
     {
         if (doorIndex == 1 && m_FirstDoor)
@@ -60,12 +68,13 @@ public class DoorController : MonoBehaviour
             Destroy(m_FirstDoor);
             m_FirstDoor = null;
         }
-        else if(doorIndex ==2 && m_SecondDoor)
+        else if (doorIndex == 2 && m_SecondDoor)
         {
             Destroy(m_SecondDoor);
-            m_FirstDoor = null;
+            m_SecondDoor = null;
         }
     }
+    //reset index && instantiate doors
     public void InitDoors()
     {
         doorIndex = 0;
@@ -73,11 +82,5 @@ public class DoorController : MonoBehaviour
             m_FirstDoor = Instantiate(m_firstDoorPrefab);
         if (m_secondDoorPrefab)
             m_SecondDoor = Instantiate(m_secondDoorPrefab);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("space"))
-            OpenFirstDoor();
     }
 }
