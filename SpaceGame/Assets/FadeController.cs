327cfdfb3c1c5d63446a30217d356612c857fb45
kukash <469260@student.saxion.l>
2020-06-25 09:31:03 +0200

    Created a fade shader, importet  few test textures

---
+++ SpaceGame/Assets/FadeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Renderer))]
+public class FadeController : MonoBehaviour
+{
+    private Renderer m_R = null;
+    [SerializeField] private float m_speed = 1.0f;
+    [SerializeField] private LeanTweenType m_TweenType = LeanTweenType.linear;
+    [SerializeField] private float m_maxFade = 1.2f;
+
+    private void Awake()
+    {
+        m_R = GetComponent<Renderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("space")) Fade();
+    }
+    public void Fade()
+    {
+        Debug.Log("Fading");
+        LeanTween.value(gameObject, 0, m_maxFade, m_speed).setEase(m_TweenType).setOnUpdate((float val) =>
+        {
+            m_R.material.SetFloat("FADE", val);
+            if (val >= m_maxFade - 0.01f) FadeOut();
+        });
+    }
+    private void FadeOut()
+    {
+        LeanTween.value(gameObject, m_maxFade, 0, m_speed).setEase(m_TweenType).setOnUpdate((float val) =>
+          {
+              m_R.material.SetFloat("FADE", val);
+          });
+    }
+}
